581f47b7be845b4c8d8f4a0efca6c22f8dbd1f18
AresKarry <105958567+AresKarry@users.noreply.github.com>
2022-10-27 15:55:50 +0800

    Add files via upload

---
+++ src/pages/Center/GruopOrder/GroupOrder.vue
@@ -0,0 +1,13 @@
+<template>
+<div>我是GroupOrder组件，但我现在没有数据</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home/IndexHome.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- 使用三级联动组件，三级联动已经注册为全局组件，因此不需要引入了 -->
+    <TypeNav/>
+    <ListContainer/>
+    <RecommendIndex/>
+    <ShopRank/>
+    <GuessLike/>
+    <ShopFllor v-for="(floor) in floorList" :key="floor.id" :list="floor" />
+    <ShopBrand/>
+  </div>
+</template>
+
+<script>
+  // 引入其他组件
+  import ListContainer from '@/pages/Home/ListContainer/ListContainer.vue'
+  import RecommendIndex from '@/pages/Home/ReCommend/RecommendIndex.vue'
+  import ShopRank from '@/pages/Home/ShopRank/ShopRank.vue'
+  import GuessLike from '@/pages/Home/GuessLike/GuessLike.vue'
+  import ShopFllor from '@/pages/Home/ShopFllor/ShopFllor.vue'
+  import ShopBrand from '@/pages/Home/ShopBrand/ShopBrand.vue'
+  import {mapState} from 'vuex'
+
+  
+  export default {
+    components:{
+      ListContainer,
+      RecommendIndex,
+      ShopRank,
+      GuessLike,
+      ShopFllor,
+      ShopBrand
+    },
+    computed:{
+    },
+    mounted(){
+      // 派发action，获取floor数据
+      this.$store.dispatch('getFloorList')
+
+
+      
+    },
+    computed:{
+      ...mapState({
+        floorList:(state)=>{
+          return state.home.floorList
+        }
+      })
+    },
+  }
+</script>
+
+<style>
+
+</style>
